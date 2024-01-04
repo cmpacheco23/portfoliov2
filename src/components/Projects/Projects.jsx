975f81c35189a48c3d984d8ba0e52e01de326906
cmpacheco23 <cmpachecomuevecela@gmail.com>
2024-01-04 18:01:45 -0500

    add some content to projects page

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,19 @@
 const Projects = () => {
   return (
-    <>
-    </>
+    <div>
+      <h1>Projects</h1>
+      <div>
+        <div>
+          <img src="" alt="" />
+          <h4>Sensi Food</h4>
+          <p>Full-stack food sensitivity tracker app that streamlines allergen tracking and dietary adjustments while fostering community engagement</p>
+          <div>
+            <h4>More</h4>
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
