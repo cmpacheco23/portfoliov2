881a102e505e6268b70e9fd2c230a567fa3d9a23
cmpacheco23 <cmpachecomuevecela@gmail.com>
2024-01-04 17:05:29 -0500

    create nav bar for desktop and complete set up

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,36 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import {IoMdMenu, IoMdClose} from "react-icons/io"
+import { AnimatePresence, motion } from "framer-motion";
+
 const NavBar = () => {
+  const [open, setOpen] = useState(false)
+  return ( 
+    <header>
+      <div>
+        <div>
+          <NavLink to="/#projects"> Projects</NavLink>
+          <NavLink to="/#experience"> Projects</NavLink>
+          <NavLink to="/#skills"> Projects</NavLink>
+          <NavLink to="/#contact"> Projects</NavLink>
+        </div>
+        <div onClick={() => setOpen(true)}>
+          <IoMdMenu/>
+        </div>
+      <AnimatePresence>
+        {open && <SideBarMenu setOpen={setOpen}/>}
+      </AnimatePresence>
+      </div>
+    </header>
+  );
+}
+
+export default NavBar
+
+const SideBarMenu = ({setOpen}) => {
   return ( 
     <>
     </>
   );
 }
 
-export default NavBar
