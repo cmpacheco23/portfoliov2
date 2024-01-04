95d42b679537ac68b3d1c90c83da20c0db34bd81
cmpacheco23 <cmpachecomuevecela@gmail.com>
2024-01-04 16:52:19 -0500

    set up components and pages structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom'
+
 import './App.css'
+import HomePage from './pages/HomePage'
+import NavBar from './components/NavBar/NavBar'
+import Projects from './components/Projects/Projects'
+import Experience from './components/Experience/Experience'
+import Skills from './components/Skills/Skills'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/#projects" element={<Projects/>} />
+        <Route path="/#experience" element={<Experience/>} />
+        <Route path="/#skills" element={<Skills/>} />
+        <Route path="/#contact" element={<Contact/>} />
+      </Routes>
+      <Footer/>
+    </main>
   )
 }
 
